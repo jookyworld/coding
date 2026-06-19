59d9fddd73a8a384ca20130cf1cae52579908ad5
jooky <jooky0327@naver.com>
2026-06-19 10:07:45 +0900

    정렬 H-Index

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,36 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) throws Exception {
 
-        int[] A = {3, 30, 34, 5, 9};
-        String result = solution(A);
+        int[] A = {3, 0, 6, 1, 5};
+        int[] B = {5, 1, 6, 7, 2, 2, 2};
+        int[] C = {0,0,0,0,0};
+        int result = solution(C);
 
         System.out.println(result);
     }
 
     /*
-
-
+    정렬 시켜놓고 반복문 돌면서 찾으면 끝 아닌가..?
+    6 5 3 1 0
+    7 6 5 2 2 2 1
+    2 2 2 2 1 1
+    1 1 1 0 0
+    0 0 0
      */
 
-    public static String solution(int[] numbers) {
-        String[] tmp = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            tmp[i] = String.valueOf(numbers[i]);
-        }
-        Arrays.sort(tmp, (a, b) -> (b + a).compareTo(a + b));
+    public static int solution(int[] citations) {
+        Arrays.sort(citations);
+        int count = 1;
 
-        StringBuilder sb = new StringBuilder();
-        for (String s : tmp) {
-            sb.append(s);
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] == count) {
+                return count;
+            } else if (citations[i] < count) {
+                return count -1;
+            }
+            count++;
         }
-        String answer = sb.toString();
-
-        if(answer.startsWith("0")) return "0";
-        return answer;
+        return count - 1;
     }
 
 
